495dd87a6f28157dd1754344856fb9a6dad8e3ac
unknown <mateuszwrycza3@gmail.com>
2021-01-06 15:51:03 +0100

    implemented firestore functionality to App

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Homepage from './pages/homepage/Homepage';
 import ShopPage from './pages/shop/Shop'
 import Header from './components/header/Header'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-import {auth} from './firebase/firebase.utils'
+import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 
 import { Switch, Route} from 'react-router-dom';
 
@@ -19,8 +19,20 @@ class App extends Component {
   unsubcribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubcribeFromAuth = auth.onAuthStateChanged(user => { // metoda przyjmująca obiekt user z obiektu auth jako parametr. W funkcji zmieniamy stan na tego użytkownika z obiektu auth. Jest to tzw open Subscription co oznacza ze nasza aplikacja komunikuje sie z firtebase i np jezeli sie nie wylogujemy z tego uzytkownika to on bedzie zapamietany iw racając do paplikacji bedziemy ciagle zalogoani na tym koncie.
-      this.setState({currentUser: user})
+    this.unsubcribeFromAuth = auth.onAuthStateChanged(async userAuth => { // metoda przyjmująca obiekt user z obiektu auth jako parametr. W funkcji zmieniamy stan na tego użytkownika z obiektu auth. Jest to tzw open Subscription co oznacza ze nasza aplikacja komunikuje sie z firtebase i np jezeli sie nie wylogujemy z tego uzytkownika to on bedzie zapamietany iw racając do paplikacji bedziemy ciagle zalogoani na tym koncie.
+      if(userAuth) { // sprawdzamy czu userAuth istnieje.
+        const userRef = await createUserProfileDocument(userAuth) // z funckji createUserProfiuleDocument zwracamy userRef i ten userRef staje się tu tą zmienną. Jest to obiekt danego Użytkownika
+        userRef.onSnapshot(snapShot => { // nasług\chujemy czy snapshot obiektu userRef się zmienił. W tej funkcji za każdym razem kiedy snapShot się zmieni aktualizujemy stan. Czyli np jak przeloguje się użytkownik
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data() // rozprzestrzeniamy resztę danych obiektuSnapshot. 
+            }
+          })
+        })
+      } else  {
+        this.setState({currentUser: null}) // kiedy użytkownik sie wyloguje stan zmieniamy na null
+      }
     }) 
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,8 +1,8 @@
 import firebase from 'firebase/app' // importujemy firebase app dla projektów webowych
-import 'firebase/firestore' // tu importujemy interesujące nas usłgi czyli firestory i authenticaiton. Dzięki temu mamy dostęp do firebase.firestore() które niżej exportujemy o będą nam potrzebne 
+import 'firebase/firestore' // tu importujemy interesujące nas usłgi czyli firestore i authenticaiton. Dzięki temu mamy dostęp do firebase.firestore() które niżej exportujemy o będą nam potrzebne 
 import 'firebase/auth'
 
-const config = { //obiekt konfiguracyjny z którym inicjalizujemy Aplikację firebase.
+const config = { //obiekt konfiguracyjny otrzymany przy zakłądaniu projektu,  z którym inicjalizujemy Aplikację firebase.
     apiKey: "AIzaSyDwIb0f8PT2jxAROM_KM6XHLAiNjlWWgUs",
     authDomain: "crwn-db-ba9c9.firebaseapp.com",
     projectId: "crwn-db-ba9c9",
@@ -13,11 +13,36 @@ const config = { //obiekt konfiguracyjny z którym inicjalizujemy Aplikację fir
 
   firebase.initializeApp(config); // inicjalizujemy Applikację z podaną w parametrze konfiguracją
 
+  export const createUserProfileDocument = async (userAuth, additionalData) => { // parametr userAuth to obiekt auth użytkownika który dostajemy po zalogowaniu się przez google sign in
+    if(!userAuth) return; // jeżeli userAuth nie istnieje to wychodzimyz tej funkcji
+    const userRef = await firestore.doc(`users/${userAuth.uid}`) // wyciągamy documentReference z kolekcji users i obiekt użytkownika o podanym id
+    const snapShot = await userRef.get(); // wyciągamy z userRef snapShot object który pozwoli nam na określenie czy w tym obiekcie znajdują się jakieś dane. W tym obeikcie znajduje się właściwośc exist o wartości true lub false w zależnosci czy dany obiekt istnieje w bazie danych 
+
+    console.log(snapShot)
+    if(!snapShot.exist) { // jeżeli dane we wskazanym miejscu istnieją czyli jeżeli na miejscu users/jakiesid istnieje juz cos to nie utworzy nowych danych.
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try{
+        await userRef.set({ // dodajemy nowe dane do documentReference
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+      } catch(err){ 
+          console.log('error creating user', err.message)
+      }
+    }
+
+    return userRef; // zwracamy userRef bo moze zaistnieć potrzeba użycia tego obiektu w innym miejscu
+  }
+
   export const auth = firebase.auth(); // auth staje się biblioteką auth wyciągniętą z firebase
   export const firestore = firebase.firestore(); // firstore staje sie biblioteką firestore wyciągniętą z firebae
 
 
-const provider = new firebase.auth.GoogleAuthProvider(); // tworzymy nową klasę provider która ma funkcjonalność klasy GoogleAuthProvider, która jest wyciągnięta z klasy firebase i biblioteki/obiektu auth.
+const provider = new firebase.auth.GoogleAuthProvider(); // tworzymy nową klasę / obiekt zdefiniowany - provider która ma funkcjonalność klasy GoogleAuthProvider, która jest wyciągnięta z klasy firebase i biblioteki/obiektu auth.
 provider.setCustomParameters({ prompt: 'select_account' }) // provider czyli klasa z funkcjonalnością GoogleAuthProvider przyjmuje  parametry za pomocą metody setCustomParameters. Istnieje wiele różnych rodzajów parametrów które możemy tu przekazać. Tu przekazujemy parametr, który mówi że chcemy aby przy każdym wykorzystaniu klasy googleAuth pojawiał sie popup wyboru konta z google.
 
 export const signInWithGoogle = () => { // eksportujemy funkcję która  wywołuje metodę z biblioteki auth   która jako parametr przyjmuje nasz provider.
